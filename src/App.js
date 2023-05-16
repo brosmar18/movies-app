6e68e6899b44326e8155684c5923f2e915278cca
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-05-15 23:45:27 -0500

    MovieCard component created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useEffect } from 'react';
 import './App.css';
 import searchIcon from './search.svg';
+import MovieCard from './MovieCard';
 
 // 55505650
 
@@ -41,18 +42,7 @@ const App = () => {
                 />
             </div>
             <div className='container'>
-                <div className='movie'>
-                    <div>
-                        <p>{movie1.Year}</p>
-                    </div>
-                    <div>
-                        <img src={ movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title} />
-                    </div>
-                    <div>
-                        <span>{movie1.Type}</span>
-                        <h3>{movie1.Title}</h3>
-                    </div>
-                </div>
+                <MovieCard movie1={movie1} />
             </div>
         </div>
     );
